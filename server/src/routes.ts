702c3272647ff3e9540341943661b8a547b91e0b
João Henrique <joaohenrique.hs@hotmail.com>
2020-06-04 13:35:19 -0300

    feat: Add static files route

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,10 +1,19 @@
-import express, {Router} from 'express'
+import { Router } from 'express';
+import knex from './database/connection';
 
-const routes = express.Router();
+const routes = Router();
 
-routes.get('/', (request, respose) => {
-  console.log('Received');
-  return respose.json({ message: "Hello World!" });
+routes.get('/items', async (request, response) => {
+  const items = await knex('items').select('*');
+
+  const serializedItems = items.map(item => {
+    return {
+      title: item.title,
+      image_url: `http://localhost:3333/uploads/${item.image}`,
+    }
+  })
+
+  return response.json(serializedItems);
 });
 
 export default routes;
